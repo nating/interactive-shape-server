55080165e4ed6d1a319ea8c598d13dd82806865b
nating <nating@tcd.ie>
2017-11-22 12:50:51 +0000

    Added file serving code to assignment-1.

--- assignment-1/ShapeExample1-0.1.0.0/src/Main.hs
+++ assignment-1/ShapeExample1-0.1.0.0/src/Main.hs
@@ -1,9 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+
+import Web.Scotty
+import Data.Monoid (mconcat)
+import Text.Blaze.Svg11 ((!), mkPath, rotate, l, m)
+import qualified Text.Blaze.Svg11 as S
+import qualified Text.Blaze.Svg11.Attributes as A
+import Text.Blaze.Svg.Renderer.Utf8 (renderSvg)
 
 import Shapes
 import Render (render,defaultWindow)
-import Server (serve)
 
-exampleDrawing =  [ (scale (point 0.5 0.25) <+> translate (point 1.2 0.4), circle) ]
+svg drawing = do
+    setHeader "Content-Type" "image/svg+xml"
+    setHeader "Vary" "Accept Encoding"
+    raw $ renderSvg drawing
 
-main = serve
+{-
+
+main = scotty 3000 $ do
+  get "/:word" $ svg svgDoc
+-}
+main = scotty 3000 $ do
+  get "/" $ file "static/index.html"
+  get "/example" $ file "static/example.html"
+
+
+svgDoc :: S.Svg
+svgDoc = S.docTypeSvg ! A.version "1.1" ! A.width "500" ! A.height "500" ! A.viewbox "0 0 5 5" $ do
+    S.g ! A.transform makeTransform $ do
+      S.circle ! A.width "1" ! A.height "2" ! A.fill "#008d46"
+      S.rect ! A.width "1" ! A.height "2" ! A.fill "#ffffff"
+      S.rect ! A.width "1" ! A.height "2" ! A.fill "#d2232c"
+      S.path ! A.d makePath
+
+makePath :: S.AttributeValue
+makePath = mkPath $ do
+  l 2 3
+  m 4 5
+
+makeTransform :: S.AttributeValue
+makeTransform = S.rotate 50
